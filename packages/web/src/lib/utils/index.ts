09c402b7bbc89fbc5573cd0ab4b6c12e79c95ec8
Styles <kartik100100@gmail.com>
2026-02-16 19:38:19 +0530

    fixed shadcn utils

--- packages/web/src/lib/utils/index.ts
+++ packages/web/src/lib/utils/index.ts
@@ -1,1 +1,15 @@
-export * from "./ui";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+	return twMerge(clsx(inputs));
+}
+
+export function truncateText(text: string, length: number = 20) {
+	if (text.length <= length) return text;
+	return `${text.slice(0, length)}...`;
+}
+
+export function truncateAddress(address: string) {
+	return `${address.slice(0, 6)}...${address.slice(-4)}`;
+}
