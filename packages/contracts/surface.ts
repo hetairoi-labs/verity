c8b9e19ebb8b419c2266b1ab84e3bcefe3c9b421
Spandan Barve <spandan567@gmail.com>
2026-03-03 23:45:46 +0530

    Add packages/contracts/surface.ts

---
+++ packages/contracts/surface.ts
@@ -0,0 +1,34 @@
+import { type Client, getContract } from "viem";
+import { definitions } from "./definitions.gen";
+
+export function getContracts(
+	networkName: keyof typeof definitions,
+	client: Client,
+) {
+	if (!definitions[networkName]) {
+		throw new Error(
+			`No contract definitions found for network: ${networkName}`,
+		);
+	}
+
+	const Manager = getContract({
+		...definitions[networkName].KXManager,
+		client,
+	});
+
+	const SessionRegistry = getContract({
+		...definitions[networkName].KXSessionRegistry,
+		client,
+	});
+
+	const USDC = getContract({
+		...definitions[networkName].USDC,
+		client,
+	});
+
+	return {
+		Manager,
+		SessionRegistry,
+		USDC,
+	};
+}
