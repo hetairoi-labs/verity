12b2a003729c77e01e4c4960da0611ed7f273b5a
Spandan Barve <spandan567@gmail.com>
2026-03-05 20:58:06 +0530

    create evmcontext

---
+++ packages/web/src/lib/context/evmContext.tsx
@@ -0,0 +1,58 @@
+import { getContracts } from "@verity/contracts";
+import {
+	createContext,
+	useContext,
+	useEffect,
+	useMemo,
+	useRef,
+	useState,
+} from "react";
+import { useWalletClient } from "wagmi";
+
+type KXContracts = ReturnType<typeof getContracts>;
+
+interface IEvmContext {
+	contracts: KXContracts | null;
+	ready: boolean;
+}
+
+const EvmContext = createContext<IEvmContext>({
+	contracts: null,
+	ready: false,
+});
+
+export function EvmProvider(props: { children: React.ReactNode }) {
+	const { children } = props;
+
+	const [contracts, setContracts] = useState<KXContracts | null>(null);
+	const [ready, setReady] = useState(false);
+
+	const { data: wallet } = useWalletClient();
+
+	const flag = useRef(false);
+
+	useEffect(() => {
+		if (!flag.current && wallet) {
+			flag.current = true;
+			const kxContracts = getContracts("test", wallet);
+			setContracts(kxContracts);
+			setReady(true);
+		} else {
+			setReady(true);
+		}
+	}, [wallet]);
+
+	const value: IEvmContext = useMemo(
+		() => ({
+			ready,
+			contracts,
+		}),
+		[ready, contracts]
+	);
+
+	return <EvmContext.Provider value={value}>{children}</EvmContext.Provider>;
+}
+
+export function useEvmContext() {
+	return useContext(EvmContext);
+}
